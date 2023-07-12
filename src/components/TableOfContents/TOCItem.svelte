069c69f5d1f7a95c2a4f646bb8400d1d753a1fc3
DavidC <davidcredo00@gmail.com>
2023-07-12 15:29:44 +0200

    added blog post layout and some cleaning up

---
+++ src/components/TableOfContents/TOCItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { MarkdownHeading } from "astro";
+
+  export let heading: MarkdownHeading;
+</script>
+
+<a href={"#" + heading.slug}>{heading.text}</a>
+
+<style>
+  a {
+    margin-left: 1rem;
+    text-decoration: none;
+    color: var(--color-text-light);
+  }
+</style>
---
+++ src/components/TableOfContents/TableOfContents.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { MarkdownHeading } from "astro";
+  import TocItem from "./TOCItem.svelte";
+
+  export let headings: MarkdownHeading[];
+</script>
+
+<nav>
+  <div class="flex-wrapper">
+    <h2>Table of Contents</h2>
+    {#each headings as heading}
+      <TocItem {heading} />
+    {/each}
+  </div>
+</nav>
+
+<style>
+  .flex-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+  }
+</style>
