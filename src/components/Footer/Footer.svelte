777fe9d28b19c6801421166e503a5f7387083065
DavidC <davidcredo00@gmail.com>
2023-07-15 18:44:39 +0200

    added simple footer component

---
+++ src/components/Footer/Footer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const getCopyrightYear = (): string => {
+    const establishedYear = new Date("2023-01-01").getFullYear();
+    const currentYear = new Date().getFullYear();
+    return currentYear === establishedYear
+      ? "2023"
+      : `${establishedYear} - ${currentYear}`;
+  };
+</script>
+
+<footer>
+  <div class="container">
+    <p><strong>David Credo</strong> © {getCopyrightYear()}</p>
+  </div>
+</footer>
+
+<style>
+    
+</style>
